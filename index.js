3a33a5e2c1e97b91478bf624c53c2986ba90ffde
Yurei7 <114104437+Yurei7@users.noreply.github.com>
2023-05-26 19:11:53 -0400

    refactoring and new section added

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+const explorerCard= document.querySelector(".explorer");
+explorerCard.addEventListener("mouseover", function (){
+    explorerCard.classList.remove("explorer")
+    explorerCard.classList.add("mouseoverExplorer")
+})
+explorerCard.addEventListener('mouseout',function (){
+    explorerCard.classList.remove("mouseoverExplorer");
+    explorerCard.classList.add("explorer");
+})
+*/
